e33651aff7e5f89874ac870a731722013af92898
km-saifullah <kmsaifullah16@gmail.com>
2024-06-07 12:14:02 +0600

    added TextInput field style and some other stuffs like keyboard type and so on

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Rest</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,4 +18,28 @@ const StartGameScreen = () => {
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
