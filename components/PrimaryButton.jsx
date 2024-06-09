ee521936844c4a9055a29669a67cf2e75750740b
km-saifullah <kmsaifullah16@gmail.com>
2024-06-09 09:16:58 +0600

    primary button design done

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 
 const PrimaryButton = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonContainer, styles.pressedIOS]
+            : styles.buttonContainer
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+    
+  },
+  buttonContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  btnText: {
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  pressedIOS: {
+    opacity: 0.75,
+  },
+});
